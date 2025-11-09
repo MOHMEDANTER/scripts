f4e4ede4021bc0c634a32a7e78f54776b2549893
MOHMEDANTER <mohameddanterr@gmail.com>
2025-11-09 14:14:33 +0200

    view tasks

--- to_do_list.py
+++ to_do_list.py
@@ -6,3 +6,22 @@ def add_task(task,status):
 
 add_task("cut","Pending")
 
+def view():
+  try:
+    if todo_list:
+      for idx,item in enumerate(todo_list,1):
+        task = item['task']
+        status = item['status']
+      # for i,ii in enumerate(todo_list,1):
+        print(idx,item)
+    else:
+      print("No tasks in the to-do list.")
+
+  except:
+    print("Invalid input. Please enter a valid number.")
+
+
+  print(f"( {idx} )::::( {task} )::::( {status} )")
+
+  print("---------------------------------------\n")
+
