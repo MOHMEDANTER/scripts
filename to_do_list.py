f4df84c3525b35f6d8fdcae3cbdbaa631dc3ecc6
MOHMEDANTER <mohameddanterr@gmail.com>
2025-11-09 14:28:44 +0200

    run function

--- to_do_list.py
+++ to_do_list.py
@@ -46,3 +46,57 @@ def delet(delet_num):
     print(f"The metion ( {delet_num} ) is ( {deleted_task['task']} ) and the status was ( {deleted_task["status"]} ) already deleted .")
   else:
       print("Invalid task number")
+
+
+def run():
+  while True:
+    print("\n-----------------Task manager---------------------------\n")
+    print('1--> Add Task')
+    print('2--> View Task')
+    print('3--> Delete Task')
+    print('4--> modify pending to done')
+    print('5--> Exit')
+    print("\n--------------------------------------------\n")
+    choice = int(input('Enter your choice: '))
+    if choice == 1:
+      task = str(input('Enter the task: ')).title()
+      status = str(input('Enter the status: ')).title()
+      add_task(task,status)
+      view()
+    elif choice == 2:
+      view()
+    elif choice == 3:
+      view()
+      if todo_list:
+        num = input('Enter the number of the task : ')
+        delet(num)
+    elif choice == 4:
+      view()
+      mod = input("Enter the number of the do you want to modify : --> ")
+      modify(mod)
+
+      print("congratulation the task modified to done")
+      view()
+    elif choice == 5:
+      print("thank you for using our app")
+      break
+    else:
+      print('Invalid choice')
+
+if __name__ == "__main__":
+  run()
+
+print (todo_list)
+
+
+
+
+
+
+
+
+
+
+
+
+      
